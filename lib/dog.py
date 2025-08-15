94c23743f6e7f73b277db94af4b286a0ef237c06
KAYABINGHI <muchaipauls@gmail.com>
2025-08-15 15:22:58 +0300

    attirbutes & props

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,39 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
+# lib/dog.py
+
 class Dog:
-    pass
+    approved_breeds = [
+        "Mastiff", "Chihuahua", "Corgi", "Shar Pei",
+        "Beagle", "French Bulldog", "Pug", "Pointer"
+    ]
+
+    def __init__(self, name="Unknown", breed="Mastiff"):
+        self.name = name      # use property setter
+        self.breed = breed    # use property setter
+
+    # --- name property ---
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # --- breed property ---
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in Dog.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
